46432381533c28791afdf8c3d6b6c6b7e58ef4df
Tjark Weber <tjark.weber@gmx.de>
2016-02-26 16:20:51 +0100

    Isar proof.

--- Bisimilarity_Implies_Equivalence.thy
+++ Bisimilarity_Implies_Equivalence.thy
@@ -11,6 +11,46 @@ by (metis permute_finite supp_eqvt)
 context indexed_nominal_ts
 begin
 
+  lemma bisimilarity_implies_equivalence_Act:
+    assumes "\<And>P Q. P \<sim>\<cdot> Q \<Longrightarrow> P \<Turnstile> x \<longleftrightarrow> Q \<Turnstile> x"
+    and "P \<sim>\<cdot> Q"
+    and "P \<Turnstile> Act \<alpha> x"
+    shows "Q \<Turnstile> Act \<alpha> x"
+  proof -
+    from `P \<Turnstile> Act \<alpha> x` obtain \<alpha>' x' P' where eq: "Act \<alpha> x = Act \<alpha>' x'" and trans: "P \<rightarrow> \<langle>\<alpha>',P'\<rangle>" and valid: "P' \<Turnstile> x'"
+      unfolding valid_Act by metis
+    from eq obtain p where 1: "x' = p \<bullet> x"
+      apply (auto simp add: Act_eq_iff Act\<^sub>\<alpha>_eq_iff alphas)
+      apply (metis Rep_formula_inverse Tree\<^sub>\<alpha>.abs_eq_iff Tree\<^sub>\<alpha>_abs_rep permute_Tree\<^sub>\<alpha>.abs_eq permute_formula.rep_eq)
+      done
+    -- \<open>rename~@{term "Act \<alpha>' x'"} to avoid~@{term Q}, without touching~@{term "\<langle>\<alpha>',P'\<rangle>"}\<close>
+    obtain q where 2: "(q \<bullet> bn \<alpha>') \<sharp>* Q" and 3: "supp (Act \<alpha>' x', \<langle>\<alpha>',P'\<rangle>) \<sharp>* q"
+      proof (rule at_set_avoiding2[of "bn \<alpha>'" Q "(Act \<alpha>' x', \<langle>\<alpha>',P'\<rangle>)", THEN exE])
+        show "finite (bn \<alpha>')" by (fact bn_finite)
+      next
+        show "finite (supp Q)" by (fact finite_supp)
+      next
+        show "finite (supp (Act \<alpha>' x', \<langle>\<alpha>',P'\<rangle>))" by (simp add: finite_supp supp_Pair)
+      next
+        show "bn \<alpha>' \<sharp>* (Act \<alpha>' x', \<langle>\<alpha>',P'\<rangle>)"
+          by (metis (no_types, lifting) Diff_iff bn_abs_residual_fresh fresh_def fresh_star_Pair fresh_star_def supp_Act)
+      qed metis
+    from eq and 3 have eq': "Act \<alpha> x = Act (q \<bullet> \<alpha>') (q \<bullet> x')"
+      using fresh_star_supp_conv perm_supp_eq by fastforce
+    from 3 have "\<langle>\<alpha>',P'\<rangle> = \<langle>q \<bullet> \<alpha>',q \<bullet> P'\<rangle>"
+      by (metis abs_residual_pair_eqvt fresh_star_Un supp_Pair supp_perm_eq)
+    with `P \<sim>\<cdot> Q` and 2 and trans obtain Q' where trans': "Q \<rightarrow> \<langle>q \<bullet> \<alpha>',Q'\<rangle>" and "(q \<bullet> P') \<sim>\<cdot> Q'"
+      by (metis bisimilar_simulation_step bn_eqvt)
+    then have "(-p \<bullet> P') \<sim>\<cdot> (-p \<bullet> -q \<bullet> Q')"
+      by (metis bisimilar_eqvt permute_minus_cancel(1))
+    moreover from 1 and valid have "-p \<bullet> P' \<Turnstile> x"
+      by (metis permute_minus_cancel(1) valid_eqvt)
+    ultimately have "Q' \<Turnstile> q \<bullet> x'"
+      using 1 and `\<And>P Q. P \<sim>\<cdot> Q \<Longrightarrow> P \<Turnstile> x \<longleftrightarrow> Q \<Turnstile> x` by (metis permute_minus_cancel(2) valid_eqvt)
+    with eq' and trans' show "Q \<Turnstile> Act \<alpha> x"
+      unfolding valid_Act by metis
+  qed
+
   theorem bisimilarity_implies_equivalence: assumes "P \<sim>\<cdot> Q" shows "P =\<cdot> Q"
   unfolding logically_equivalent_def proof
     fix x :: "('idx, 'pred, 'act) formula"
@@ -22,74 +62,11 @@ begin
       case Not then show ?case
         by simp
     next
-      case Pred then moreover have "Q \<sim>\<cdot> P"
-        by (metis bisimilar_symp sympE)
-      ultimately show ?case
-        by (auto simp add: bisimilar_def is_bisimulation_def)
+      case Pred then show ?case
+        by (metis bisimilar_is_bisimulation is_bisimulation_def symp_def valid_Pred)
     next
       case (Act \<alpha> x) then show ?case
-      apply (auto simp add: valid_Act)
-      (*1*)
-       apply (subgoal_tac "\<exists>q. (q \<bullet> bn \<alpha>') \<sharp>* Q \<and> supp (Act \<alpha>' x', \<langle>\<alpha>', P'\<rangle>) \<sharp>* q")
-        prefer 2
-        apply (rule at_set_avoiding2)
-           apply (metis bn_finite)
-          apply (metis finite_supp)
-         apply (metis finite_UnI finite_supp finite_supp_abs_residual_pair supp_Pair)
-        apply (auto simp add: fresh_star_def fresh_def supp_Pair supp_abs_residual_pair)[1]
-       apply auto[1]
-       apply (rule_tac x="q \<bullet> \<alpha>'" in exI)
-       apply (rule_tac x="q \<bullet> x'" in exI)
-       apply (rule conjI)
-        apply (metis Act_eqvt fresh_star_Un supp_Pair supp_perm_eq_test)
-       apply (subgoal_tac "\<langle>\<alpha>',P'\<rangle> = \<langle>q \<bullet> \<alpha>',q \<bullet> P'\<rangle>")
-        prefer 2
-        apply (metis abs_residual_pair_eqvt fresh_star_Un supp_Pair supp_perm_eq_test)
-       apply simp
-       apply (erule_tac \<alpha>="q \<bullet> \<alpha>'" and P'="q \<bullet> P'" in bisimilar_simulation_step)
-         apply (metis bn_eqvt)
-        apply assumption
-       apply (rule_tac x=Q' in exI)
-       apply (rule conjI)
-        apply assumption
-       apply (auto simp add: Act_eq_iff Act\<^sub>\<alpha>_eq_iff alphas)[1]
-       apply (rename_tac p')
-       apply (subgoal_tac "p' \<bullet> x = x'")
-        apply (metis bisimilar_eqvt permute_minus_cancel(2) valid_eqvt)  -- \<open>patience\<close>
-       apply (metis Rep_formula_inverse Tree\<^sub>\<alpha>.abs_eq_iff Tree\<^sub>\<alpha>_abs_rep permute_Tree\<^sub>\<alpha>.abs_eq permute_formula.rep_eq)
-      (*2*)
-      apply (subgoal_tac "\<exists>q. (q \<bullet> bn \<alpha>') \<sharp>* P \<and> supp (Act \<alpha>' x', \<langle>\<alpha>', P'\<rangle>) \<sharp>* q")
-       prefer 2
-       apply (rule at_set_avoiding2)
-          apply (metis bn_finite)
-         apply (metis finite_supp)
-        apply (metis finite_UnI finite_supp finite_supp_abs_residual_pair supp_Pair)
-       apply (auto simp add: fresh_star_def fresh_def supp_Pair supp_abs_residual_pair)[1]
-      apply auto[1]
-      apply (rule_tac x="q \<bullet> \<alpha>'" in exI)
-      apply (rule_tac x="q \<bullet> x'" in exI)
-      apply (rule conjI)
-       apply (metis Act_eqvt fresh_star_Un supp_Pair supp_perm_eq_test)
-      apply (subgoal_tac "\<langle>\<alpha>',P'\<rangle> = \<langle>q \<bullet> \<alpha>',q \<bullet> P'\<rangle>")
-       prefer 2
-       apply (metis abs_residual_pair_eqvt fresh_star_Un supp_Pair supp_perm_eq_test)
-      apply simp
-      apply (subgoal_tac "Q \<sim>\<cdot> P")
-       prefer 2
-       apply (metis bisimilar_symp sympE)
-      apply (thin_tac "P \<sim>\<cdot> Q")
-      apply (erule_tac \<alpha>="q \<bullet> \<alpha>'" and P'="q \<bullet> P'" in bisimilar_simulation_step)
-        apply (metis bn_eqvt)
-       apply assumption
-      apply (rule_tac x=Q' in exI)
-      apply (rule conjI)
-       apply assumption
-      apply (auto simp add: Act_eq_iff Act\<^sub>\<alpha>_eq_iff alphas)[1]
-      apply (rename_tac p')
-      apply (subgoal_tac "x' = p' \<bullet> x")
-       apply (metis bisimilar_eqvt permute_minus_cancel(2) valid_eqvt)   -- \<open>patience\<close>
-      apply (metis Rep_formula_inverse Tree\<^sub>\<alpha>.abs_eq_iff Tree\<^sub>\<alpha>_abs_rep permute_Tree\<^sub>\<alpha>.abs_eq permute_formula.rep_eq)
-      done
+        by (metis bisimilar_symp bisimilarity_implies_equivalence_Act sympE)
     qed
   qed
 
